b9d03958247e36be53e42fd2a2839c0676c1a2ad
Claude Ilunga <claudekjci@gmail.com>
2026-09-12 08:56:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -52,5 +52,43 @@ def db_test():
         }), 500
 
 
+@app.route("/api/init-db")
+def init_db():
+    try:
+        if not DATABASE_URL:
+            return jsonify({
+                "success": False,
+                "message": "DATABASE_URL n'est pas configurée"
+            }), 500
+
+        with psycopg.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cursor:
+
+                cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS users (
+                        id SERIAL PRIMARY KEY,
+                        full_name VARCHAR(150) NOT NULL,
+                        phone VARCHAR(30) UNIQUE NOT NULL,
+                        email VARCHAR(150) UNIQUE,
+                        password_hash VARCHAR(255) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    )
+                """)
+
+            conn.commit()
+
+        return jsonify({
+            "success": True,
+            "message": "Table users créée avec succès !"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "message": "Erreur lors de la création de la table users",
+            "error": str(e)
+        }), 500
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
